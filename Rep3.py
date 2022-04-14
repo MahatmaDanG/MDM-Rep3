863fb01aa31ea1279b01524ebe715dc53e054cac
clem-fry <98823205+clem-fry@users.noreply.github.com>
2022-04-14 23:46:27 +0100

    Update Rep3.py
    Adding a differential equation for public perception depending on stock price, dividends and change in stock price. This then affects the new stock price.

--- Rep3.py
+++ Rep3.py
@@ -19,14 +19,14 @@ def StocksOwned(I,ISP):
     Owned = I/ISP
     return(Owned) 
 
-def StockSim(RO,SO,Price):
+def StockSim(RO,SO,Price,perception):
     Severity = r.random()*SO            # Uses a random variable and a controlled waiting to control stock varition
     StockRand = r.random()              # Chooses whether stock increases or decreases
-    
+    price_t = Price*Severity*perception
     if StockRand < Stability:           
-        Price += Price*Severity
+        Price += price_t
     elif StockRand > Stability:
-        Price -= Price*Severity
+        Price -= price_t
     return(Price)
     
     
@@ -36,16 +36,50 @@ def GraphPlot(M,V):
     plt.xlabel('Minute')
     plt.ylabel('Value of Investment')
     plt.show()
+    
+def numericalDifferentiation(data_list): # differentiating discrete data
+    if len(data_list) < 2:
+        return 0
+    differential = (data_list[-1] - data_list[-2]) #Assuming a time interval of 1
+    #print(differential)
+    return differential
+
+def numericalIntegration(dataList): # integrating discrete data
+    total = 0
+    if len(dataList) == 0:
+        total = 0
+    elif len(dataList) == 1:
+        total = dataList[0]
+    else:
+        for i in range(len(dataList)-1):
+            total = total + (dataList[i] + dataList[i+1]/2) # Assuming a time interval of 1
+    #print('total:', total)
+    return total
+
+def publicPerception(div, SO_list, perception_t_list):
+    dividendsConstant = 0.00005
+    sharePriceConstant = 0.00002
+    changeInSharePriceConstant = 0.00001
+
+    publicPerception_t = dividendsConstant * div + sharePriceConstant * SO_list[-1] + numericalDifferentiation(SO_list) * changeInSharePriceConstant # equation for change in opinion
+    
+    publicPerception = numericalIntegration(perception_t_list) # integrate to find a value
+    perception_t_list.append(publicPerception_t) #A list is needed to integrate back over time
+    
+    return publicPerception, perception_t_list
 
 
-Minutes = []
-Value = []    
-
+Minutes = [0]
+Value = [Investment]    
+perception_t_list = []
 Vs_t = I_Stock_Price
 No_Owned = StocksOwned(Investment, I_Stock_Price)
 
-for loop in range(100000):                        # The total amount of minutes simulated
-    Vs_t = StockSim(Stability, Severity_Control, Vs_t)
+for loop in range(10000):                        # The total amount of minutes simulated
+    perception_info = publicPerception(DividendPercent, Value, perception_t_list) #Produces a perception value using dividends, stock value and change in stock value
+    perception_t_list = perception_info[1]
+    perception = perception_info[0]
+    Vs_t = StockSim(Stability, Severity_Control, Vs_t, perception)
     Dividend_at_t = DividendPercent * Vs_t
     Vi_t = No_Owned*(Vs_t + Dividend_at_t)
     Minutes.append(loop)
